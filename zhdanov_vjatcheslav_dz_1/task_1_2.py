d820a977dc48465975297b6827fe1b495b27e1d3
Zhdanov-Vyacheslav <alex.city.jdanov@gmail.com>
2021-09-19 12:08:25 +0300

    Lesson 1, task 2 Refactoring

--- zhdanov_vjatcheslav_dz_1/task_1_2.py
+++ zhdanov_vjatcheslav_dz_1/task_1_2.py
@@ -15,22 +15,20 @@ else:
     pool = pool // 2
 
 #  Создаем список кубов нечетных чисел
-for idx in range(pool):
+for idx in range(1, pool):
     check_multiplicity = 0
     odd_number += 2  # Получаем следующее число
     list_odd_number.append(odd_number ** arithmetic_root)  # Возводим в куб
-    string_of_number = str(list_odd_number[idx + 1])  # Переводим число в строку для дальнейших операций
-    for idx2 in range(len(string_of_number)):  # Проходим поэлементно по числу и суммируем его элементы между собой
-        check_multiplicity += int(string_of_number[idx2])
+    for number_element in str(list_odd_number[idx]):  # Проходим поэлементно по числу и суммируем его элементы
+        check_multiplicity += int(number_element)
     if check_multiplicity % denominator == 0:  # Проверка делиться ли без остатка
         result += check_multiplicity
 
-for idx in range(len(list_odd_number)):
+for idx, temp in enumerate(list_odd_number):
     check_multiplicity = 0
     list_odd_number[idx] += increment  # Прибавляем к кубу заданное значение
-    string_of_number = str(list_odd_number[idx])  # Переводим число в строку для дальнейших операций
-    for idx2 in range(len(string_of_number)):  # Проходим поэлементно по числу и суммируем его элементы между собой
-        check_multiplicity += int(string_of_number[idx2])
+    for number_element in str(list_odd_number[idx]) :  # Проходим поэлементно по числу и суммируем его элементы
+        check_multiplicity += int(number_element)
     if check_multiplicity % denominator == 0:  # Проверка делиться ли без остатка
         result_increment += check_multiplicity
 
